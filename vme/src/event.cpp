38e5dddea71c56c657bc3eb04738391d9ca4b8b7
Prool <proolix@gmail.com>
2020-08-02 22:53:12 +0300

    Manually prepare for merge

--- vme/src/event.cpp
+++ vme/src/event.cpp
@@ -27,6 +27,7 @@
 #include "textutil.h"
 #include "files.h"
 #include "event.h"
+#include "dilrun.h"
 #include <sys/time.h>
 void check_reboot_event(void *, void *);
 void delayed_action(void *p1, void *p2);
@@ -76,6 +77,31 @@ struct eventq_elem *eventqueue::add(int when, void (*func)(void *, void *),
         slog(LOG_ALL, 0, "Error: %d EVENT", when);
     }
 
+    // One could add a sanity check to make sure that events for known 
+    if (func == special_event)
+    {
+        struct unit_data *u = (struct unit_data *) arg1;
+        struct unit_fptr *f = (struct unit_fptr *) arg2;
+        membug_verify(u);
+        membug_verify(f);
+        membug_verify(f->data);
+
+        if (u)
+            assert(!u->is_destructed());
+
+        if (f)
+            assert(!f->is_destructed());
+
+        if (f->index == 82)
+        {
+            struct dilprg *prg = (struct dilprg *) f->data;
+            membug_verify(prg);
+
+            assert(prg);
+            assert(prg->owner == u);
+        }
+    }
+
     if ((count + 10) > heapsize)
     {
         if (!heapsize)
@@ -87,6 +113,7 @@ struct eventq_elem *eventqueue::add(int when, void (*func)(void *, void *),
         {
             heapsize = heapsize * 2;
             RECREATE(heap, struct eventq_elem *, heapsize);
+            slog(LOG_DIL, 0, "HEAP expand to %d", heapsize);
         }
     }
     count++;
@@ -118,24 +145,25 @@ struct eventq_elem *eventqueue::add(int when, void (*func)(void *, void *),
 void eventqueue::remove(void (*func)(void *, void *), void *arg1, void *arg2)
 {
     int i;
-    if ((func == special_event) && arg2 && (((struct unit_fptr *)arg2)->event))
+    if ((func == special_event) && arg2 && (((class unit_fptr *)arg2)->event))
     {
-        ((struct unit_fptr *)arg2)->event->func = NULL;
-        ((struct unit_fptr *)arg2)->event = NULL;
+        ((class unit_fptr *)arg2)->event->func = NULL;
+        ((class unit_fptr *)arg2)->event = NULL;
+
     }
     else if ((func == affect_beat) &&
-             arg1 && (((struct unit_affected_type *)arg1)->event))
+             arg1 && (((class unit_affected_type *)arg1)->event))
     {
-        ((struct unit_affected_type *)arg1)->event->func = NULL;
-        ((struct unit_affected_type *)arg1)->event = NULL;
+        ((class unit_affected_type *)arg1)->event->func = NULL;
+        ((class unit_affected_type *)arg1)->event = NULL;
     }
     else if ((func == affect_beat) &&
-             arg1 && (!((struct unit_affected_type *)arg1)->event))
+             arg1 && (!((class unit_affected_type *)arg1)->event))
     {
         return;
     }
     else if ((func == special_event) &&
-             arg2 && (!((struct unit_fptr *)arg2)->event))
+             arg2 && (!((class unit_fptr *)arg2)->event))
     {
         return;
     }
@@ -216,78 +244,97 @@ void eventqueue::process(void)
         if (tmp_event->func)
         {
             tfunc = tmp_event->func;
+            int bDestructed = 0;
+
             if (tfunc == special_event && ((unit_fptr *)tmp_event->arg2)->data &&
-                ((struct unit_fptr *)tmp_event->arg2)->index == SFUN_DIL_INTERNAL)
+                (((class unit_fptr *)tmp_event->arg2)->index == SFUN_DIL_INTERNAL))
             {
-                if (0/*((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->prgname*/) // prool: crash bypass #2
-                    sprintf(dilname, "%s",
-                            ((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->prgname);
-                else
-                    sprintf(dilname, "NO NAME");
-                if (0/*((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->zone*/) // prool: crash bypass (обход)
+                sprintf(dilname, "NO NAME");
+                sprintf(dilzname, "NO ZONE");
+                sprintf(diloname, "NO NAME");
+                sprintf(dilozname, "NO ZONE");
+
+                class unit_data *u = (unit_data *)tmp_event->arg1;
+                class unit_fptr *fptr = (unit_fptr *)tmp_event->arg2;
+
+                membug_verify(u);
+                membug_verify(fptr);
+
+                bDestructed = (u->is_destructed() || fptr->is_destructed());
+
+                if (!bDestructed)
                 {
-                            if (((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->zone->name) { // prool's protect
-                    //printf("prool's zonename=%s\n", ((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->zone->name);
-                    sprintf(dilzname, "%s", // prool: crash here
-                            ((dilprg *)((unit_fptr *)tmp_event->arg2)->data)->fp->tmpl->zone->name);
-			    }
-			    else {
-				printf("prool's debug: error #0\n");
-                    		sprintf(dilzname, "NO ZONE");
-			    }
+                    class dilprg *prg = (class dilprg *) fptr->data;
+
+                    assert(prg);
+                    membug_verify(prg);
+
+                    assert(prg->owner);
+                    assert(prg->fp);
+
+                    // One could check here if the DIL (if it is) is supposed to run
+                    // if (prg->waitcmd > WAITCMD_FINISH) bDestructed = 1;
+                    // But rundil checks too.
+
+                    if (prg->fp->tmpl->prgname)
+                        sprintf(dilname, "%s",
+                                prg->fp->tmpl->prgname);
+                    if (prg->fp->tmpl->zone)
+                        sprintf(dilzname, "%s",
+                                prg->fp->tmpl->zone->name);
+                    sprintf(diloname, "%s", tmp_event->arg1 ? UNIT_FI_NAME((class unit_data *)(tmp_event->arg1)) : "NO NAME");
+                    sprintf(dilozname, "%s", tmp_event->arg1 ? UNIT_FI_ZONENAME((class unit_data *)(tmp_event->arg1)) : "NO ZONE");
                 }
-                else
+            }
+
+            if (!bDestructed)
+            {
+                (tmp_event->func)(tmp_event->arg1, tmp_event->arg2);
+                loop_process++;
+                total_process++;
+                if (max_process < loop_process)
                 {
-                    sprintf(dilzname, "NO ZONE");
+                    max_process_time = loop_time;
+                    max_process = loop_process;
                 }
-                sprintf(diloname, "%s", tmp_event->arg1 ? UNIT_FI_NAME((class unit_data *)(tmp_event->arg1)) : "NO NAME");
-                sprintf(dilozname, "%s", tmp_event->arg1 ? UNIT_FI_ZONENAME((class unit_data *)(tmp_event->arg1)) : "NO ZONE");
-            }
-            (tmp_event->func)(tmp_event->arg1, tmp_event->arg2);
-            loop_process++;
-            total_process++;
-            if (max_process < loop_process)
-            {
-                max_process_time = loop_time;
-                max_process = loop_process;
-            }
 
-            gettimeofday(&pnow, (struct timezone *)0);
-            us = (pnow.tv_sec - now.tv_sec) * 1000000L + (pnow.tv_usec - now.tv_usec);
-            loop_time = us / 1000000.0;
+                gettimeofday(&pnow, (struct timezone *)0);
+                us = (pnow.tv_sec - now.tv_sec) * 1000000L + (pnow.tv_usec - now.tv_usec);
+                loop_time = us / 1000000.0;
 
-            if (loop_time > .1)
-            {
-                if (tfunc == special_event)
+                if (loop_time > .1)
                 {
-                    if (((struct unit_fptr *)tmp_event->arg2)->index == SFUN_DIL_INTERNAL)
-                        slog(LOG_DIL, 0, "Process took %1.4f seconds to complete: %s@%s on %s@%s - %s (%d)'", loop_time, dilname, dilzname, diloname, dilozname, unit_function_array[((struct unit_fptr *)tmp_event->arg2)->index].name, ((struct unit_fptr *)tmp_event->arg2)->index);
+                    if (tfunc == special_event)
+                    {
+                        if (((class unit_fptr *)tmp_event->arg2)->index == SFUN_DIL_INTERNAL)
+                            slog(LOG_DIL, 0, "Process took %1.4f seconds to complete: %s@%s on %s@%s - %s (%d)'", loop_time, dilname, dilzname, diloname, dilozname, unit_function_array[((class unit_fptr *)tmp_event->arg2)->index].name, ((class unit_fptr *)tmp_event->arg2)->index);
 
+                        else
+                            slog(LOG_DIL, 0, "Internal process took %1.4f seconds to complete: %s (%d)'",
+                                loop_time, unit_function_array[((class unit_fptr *)tmp_event->arg2)->index].name, ((class unit_fptr *)tmp_event->arg2)->index);
+                    }
                     else
-                        slog(LOG_DIL, 0, "Internal process took %1.4f seconds to complete: %s (%d)'",
-                             loop_time, unit_function_array[((struct unit_fptr *)tmp_event->arg2)->index].name, ((struct unit_fptr *)tmp_event->arg2)->index);
+                    {
+                        if (tfunc == check_reboot_event)
+                            sprintf(pname, "Reboot Event");
+                        else if (tfunc == affect_beat)
+                            sprintf(pname, "Affect Beat");
+                        else if (tfunc == delayed_action)
+                            sprintf(pname, "Affect Beat");
+                        else if (tfunc == check_idle_event)
+                            sprintf(pname, "Check Idle Event");
+                        else if (tfunc == perform_violence_event)
+                            sprintf(pname, "Violence Event");
+                        else if (tfunc == weather_and_time_event)
+                            sprintf(pname, "Weather And Time Event");
+                        else if (tfunc == zone_event)
+                            sprintf(pname, "Zone Reset Event");
+                        else
+                            sprintf(pname, "UNKNOWN Event");
+                        slog(LOG_DIL, 0, "Internal Process (%s) Took %1.4f seconds to Complete", pname, loop_time);
+                    }
                 }
-                else
-                {
-                    if (tfunc == check_reboot_event)
-                        sprintf(pname, "Reboot Event");
-                    else if (tfunc == affect_beat)
-                        sprintf(pname, "Affect Beat");
-                    else if (tfunc == delayed_action)
-                        sprintf(pname, "Affect Beat");
-                    else if (tfunc == check_idle_event)
-                        sprintf(pname, "Check Idle Event");
-                    else if (tfunc == perform_violence_event)
-                        sprintf(pname, "Violence Event");
-                    else if (tfunc == weather_and_time_event)
-                        sprintf(pname, "Weather And Time Event");
-                    else if (tfunc == zone_event)
-                        sprintf(pname, "Zone Reset Event");
-                    else
-                        sprintf(pname, "UNKNOWN Event");
-                    slog(LOG_DIL, 0, "Internal Process (%s) Took %1.4f seconds to Complete", pname, loop_time);
-                }
-            }
+            }  // !bDestructed
         }
         delete tmp_event;
         tmp_event = NULL;
